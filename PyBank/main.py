15889b7741166d9d5ef6dacc3a706ce4826f028a
pietrostolf <124335112+pietrostolf@users.noreply.github.com>
2023-03-12 20:54:57 -0400

    Completed challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,29 +1,52 @@
+#import dependencies
 import os
 import csv
 import numpy as np
 
+#create lists for dates and profit_losses data
 data = []
+date = []
 
 
-
+#define csv path
 budget_csv = os.path.join("Resources","budget_data.csv")
 
-
+#open csv
 with open(budget_csv, 'r', encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile)
     monthcount = 0
     net = 0
     next(csvfile)
+
+#iterate through csv rows and find results
     for row in csvreader:
         monthcount +=1
         net = int(row[1]) + net
         data.append(int(row[1]))
-   
+        date.append(row[0])
+
+#find changes in dataset        
     changes = np.diff(data)
     average_change = np.mean(changes)
     mean = net / monthcount
+    min_pos = np.argmin(changes)
+    max_pos = np.argmax(changes)
 
-    print(monthcount)
-    print(net)
-    print(f"Average Change:, {round(average_change,2)}")
-    
+#display results on the console
+print("Financial Analysis")
+print("-----------------------------------")
+print(f"Total Months: {monthcount}")
+print(f"Total: {net}")
+print(f"Average Change:, ${round(average_change,2)}")
+print(f"Greatest Increase in Profits: {date[max_pos+1]} (${max(changes)})")
+print(f"Greatest Decrease in Profits: {date[min_pos+1]} (${min(changes)})")
+
+#generate txt file with results
+with open('analysis/output.txt', 'w') as f:
+    f.write("Financial Analysis\n")
+    f.write("-----------------------------------\n")
+    f.write(f"Total Months: {monthcount}\n")
+    f.write(f"Total: {net}\n")
+    f.write(f"Average Change: ${round(average_change,2)}\n")
+    f.write(f"Greatest Increase in Profits: {date[max_pos+1]} (${max(changes)})\n")
+    f.write(f"Greatest Decrease in Profits: {date[min_pos+1]} (${min(changes)})\n")
---
+++ PyPoll/main.py
@@ -0,0 +1,58 @@
+#import dependencies
+import os
+import csv
+
+#declare vote variables
+votes_charles = 0
+votes_diana = 0        
+votes_raymon = 0
+votecount = 0
+
+#path for csv file
+budget_csv = os.path.join("Resources","election_data.csv")
+
+#open csv file
+with open(budget_csv, 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+
+#ignore first line    
+    next(csvfile)
+
+#iterate through csv rows to find votes counts   
+    for row in csvreader:
+        votecount +=1
+        if row[2] == "Charles Casper Stockham":
+            votes_charles += 1
+        elif  row[2] == "Diana DeGette": 
+            votes_diana += 1
+        elif  row[2] == "Raymon Anthony Doane": 
+            votes_raymon += 1
+        
+#asociate each vote counte with its respective candidate   
+vote_dic = {"Charles Casper Stockham": votes_charles, "Diana DeGette": votes_diana, "Raymon Anthony Doane": votes_raymon}           
+
+#display results on the console
+print("Election Results")
+print("-----------------------------------")
+print(f"Total Votes: {votecount}")
+print("-----------------------------------")
+print(f"Charles Casper Stockham: {round((votes_charles/votecount)*100,3)}% ({votes_charles})")
+print(f"Diana DeGette: {round((votes_diana/votecount)*100,3)}% ({votes_diana})")
+print(f"Raymon Anthony Doane: {round((votes_raymon/votecount)*100,3)}% ({votes_raymon})")
+print("-----------------------------------")
+print(f"Winner: {max(vote_dic, key=lambda k: vote_dic[k])}")
+print("-----------------------------------")
+
+#generate txt file with results
+with open('analysis/output.txt', 'w') as f:
+        f.write("Election Results\n")
+        f.write("-----------------------------------\n")
+        f.write(f"Total Votes: {votecount}\n")
+        f.write("-----------------------------------\n")
+        f.write(f"Charles Casper Stockham: {round((votes_charles/votecount)*100,3)}% ({votes_charles})\n")
+        f.write(f"Diana DeGette: {round((votes_diana/votecount)*100,3)}% ({votes_diana})\n")
+        f.write(f"Raymon Anthony Doane: {round((votes_raymon/votecount)*100,3)}% ({votes_raymon})\n")
+        f.write("-----------------------------------\n")
+        f.write(f"Winner: {max(vote_dic, key=lambda k: vote_dic[k])}\n")
+        f.write("-----------------------------------\n")
+    
